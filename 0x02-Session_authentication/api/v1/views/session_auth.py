838ff7a2f345bba192f89c62eb73ad0247e790c5
dasarentow <dasarentow@gmail.com>
2024-06-06 15:00:57 +0000

    # session authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,47 +7,48 @@ from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
+from api.v1.app import auth
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """ POST /auth_session/login
-    Return:
-      - Response
+    """
+    Log in using session authentication
     """
     user_email = request.form.get('email')
     user_pwd = request.form.get('password')
+
     if not user_email:
         return jsonify(error="email missing"), 400
     if not user_pwd:
         return jsonify(error="password missing"), 400
-    try:
-        user = User.search({"email": user_email})
-    except Exception:
+
+    users = User.search({"email": user_email})
+    if not users:
         return jsonify(error="no user found for this email"), 404
-    if not user:
-        return jsonify(error="no user found for this email"), 404
-    for u in user:
-        if u.is_valid_password(user_pwd):
-            user_id = u.id
-            from api.v1.app import auth
-            session_id = auth.create_session(user_id)
-            response = jsonify(u.to_json())
+
+    for user in users:
+        if user.is_valid_password(user_pwd):
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
             response.set_cookie(getenv('SESSION_NAME'), session_id)
             return response
         else:
             return jsonify(error="wrong password"), 401
+
     return jsonify(error="no user found for this email"), 404
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout():
-    """ DELETE /auth_session/logout
-    Return:
-      - Response
     """
-    from api.v1.app import auth
-    if auth.destroy_session(request):
-        return jsonify({}), 200
-    abort(404)
+    Log out using session authentication
+    """
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
+
+
+def see():
+    pass
